a64e85521cec5504af14af041da6605a42a8dca2
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 04:24:24 -0500

    Update CreditLine.py
    Fixed some scoping issues.

--- CreditLine.py
+++ CreditLine.py
@@ -8,8 +8,9 @@ def restart_program():
     os.execl(python, python, * sys.argv)
 # Initialize User Interface
 print "This program will ask you a few basic questions about your credit line in order to show you the real impact of your interest rates and spending habits."
-# Function for terminal's user interface
+statementBalancesString = raw_input("Please enter a list of 3 or more of your most recent statement balances.")
 cardName = raw_input("Please enter your card name (e.g. Mastercard).")
+# Function for terminal's user interface
 def APR():
     percentage = raw_input("What is your current annual APR?")
     decimalString = (percentage.strip(["%"]))
@@ -17,13 +18,13 @@ def APR():
     return float1
 # Function that determines your average utilization using prior statements
 def AvgUtilization():
-    statementBalancesString = raw_input("Please enter a list of 3 or more of your most recent statement balances.")
+    
     statementBalances = map(int, statementBalancesString.split(','))
     avgStatementBalance = (sum(statementBalances) / len(statementBalances))
 # Assign variable to value of APR() function call
 user_APR = APR()
 # Figure out user's spending limit
-spendingLimit = int(raw_input("What is your spending limit in dollars?"))
+spendingLimit = int(raw_input("What is your card's spending limit in dollars?"))
 # Change these values depending on the credit card's factors
 class CreditAccount:
     def __init__(self, cardName, user_APR, AvgUtilization, spendingLimit):
