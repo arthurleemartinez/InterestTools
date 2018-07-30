d4cf41b1024568054bcf49a376306bed1d6fbd11
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 02:21:41 -0500

    Update CreditLine.py

--- CreditLine.py
+++ CreditLine.py
@@ -1,16 +1,19 @@
-# Begin Terminal's User Interface
+# Initialize User Interface
+print "This program will ask you a few basic questions about your credit line and will tell you facts about your interest costs"
+# Functions for terminal's user interface
 def APR():
     percentage = raw_input("What is your current annual APR?")
     decimalString = (percentage.strip(["%"])):
     float1 = int(decimalString)
+    return float1
+# Assign variable to value of APR() function call
 user_APR = APR()
-    
-
+# Tell the user what to type 
 # Change these values depending on the credit card's factors
 class CreditAccount:
     def __init__(self, cardName, user_APR, AvgUtilization, spendingLimit):
         self.cardName = cardName
-        self.APR = user_APR
+        self.user_APR = user_APR
         self.AvgUtilization = AvgUtilization
         self.spendingLimit = spendingLimit
        
