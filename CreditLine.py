9061b38bdc216941a5150993b01259565a1cd863
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 02:09:12 -0500

    Update and rename IndividualFinances.py to CreditLine.py
    Add percentage conversion and APR function to variable assignment

---
+++ CreditLine.py
@@ -0,0 +1,26 @@
+# Begin Terminal's User Interface
+def APR():
+    percentage = (raw_input("What is your current annual APR?")
+    decimal1 = (percentage.strip(["%"])):
+    float1 = int(decimal)
+    
+
+# Change these values depending on the credit card's factors
+class CreditAccount:
+    def __init__(self, cardName, APR(), AvgUtilization, spendingLimit):
+        self.cardName = cardName
+        self.APR = APR()
+        self.AvgUtilization = AvgUtilization
+        self.spendingLimit = spendingLimit
+       
+Class Statements:
+    numberOfMonthlyStatements = 
+    n = numberOfMonthytStatements
+    
+    def _init_(self, 
+    # Function that determines your average utilization using prior statements
+
+# Create and store instance of user's individual card line 
+creditAccount_1 = CreditAccount("Discover", 24.74%", )
+# Display user's individual card line
+print creditAccount_1
--- IndividualFinances.py
+++
@@ -1,7 +0,0 @@
-# Change these values depending on your credit score factors
-class Account:
-    def __init__(self, x, y, b, o):
-        self.
-    
-# Create instances of account
-acc_1 = Account()
