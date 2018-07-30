3cf9cbce56f4c4d3e6b7b8e1e59ea7e4bd5dae8f
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 03:13:48 -0500

    Update CreditLine.py
    Replaced average utilization function with more efficient, user friendly implementation.

--- CreditLine.py
+++ CreditLine.py
@@ -1,16 +1,20 @@
 # Initialize User Interface
-print "This program will ask you a few basic questions about your credit line and will tell you facts about your interest costs"
-# Functions for terminal's user interface
+print "This program will ask you a few basic questions about your credit line "
+# Function for terminal's user interface
 def APR():
     percentage = raw_input("What is your current annual APR?")
     decimalString = (percentage.strip(["%"])):
     float1 = int(decimalString)
     return float1
+# Function that determines your average utilization using prior statements
+def AvgUtilization():
+    statementBalancesString = raw_input("Please enter a list of 3 or more of your most recent statement balances.")
+    statementBalances = map(int, statementBalancesString.split(','))
+    avgStatementBalance = sum(statementBalances) / len(statementBalances)
 # Assign variable to value of APR() function call
 user_APR = APR()
 # Figure out user's spending limit
 spendingLimit = int(raw_input("What is your spending limit in dollars?"))
-
 # Change these values depending on the credit card's factors
 class CreditAccount:
     def __init__(self, cardName, user_APR, AvgUtilization, spendingLimit):
@@ -18,15 +22,8 @@ class CreditAccount:
         self.user_APR = user_APR
         self.AvgUtilization = AvgUtilization
         self.spendingLimit = spendingLimit
-       
-class Statements:
-    numberOfMonthlyStatements = 
-    n = numberOfMonthytStatements
     
-    def _init_(self, 
-    # Function that determines your average utilization using prior statements
-
 # Create and store instance of user's individual card line 
-creditAccount_1 = CreditAccount("Discover", user_APR,  )
+creditAccount_1 = CreditAccount("Discover", user_APR, AvgUtilization, spendingLimit)
 # Display user's individual card line
 print creditAccount_1
