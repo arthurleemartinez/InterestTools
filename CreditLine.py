8856665d0239356cd5dfa4ec72e454640013e1cd
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 05:45:46 -0500

    Update CreditLine.py

--- CreditLine.py
+++ CreditLine.py
@@ -49,8 +49,8 @@ def confirmation():
         print("Your information will now be used to calculate certain information about your interest costs.")
     return boolean_answer
 def restart_program():
-    """Restarts the current program.
-    Note: this function does not return. Any cleanup action (like
-    saving data) must be done before calling this function."""
+    #Restarts the current program.
+    #Note: this function does not return. Any cleanup action (like
+    #saving data) must be done before calling this function.
     python = sys.executable
     os.execl(python, python, * sys.argv)
