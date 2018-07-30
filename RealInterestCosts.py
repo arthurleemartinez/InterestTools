e9d0d856128690df5d10f952ff06dc708934038e
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 07:34:52 -0500

    Update RealInterestCosts.py

--- RealInterestCosts.py
+++ RealInterestCosts.py
@@ -1,2 +1,25 @@
 # Import your credit factors from other simplified class file
 import CreditLine.py
+# Initialize User Interface
+print("This program will import your credit line information from the last program and calculate costs related to interest.")
+class Interest:
+    monthly_apr = user_APR / 12
+    monthly_interest = monthly_apr * (user_AvgUtilization * spendingLimit)
+
+class Utilization:
+    def __init__(self, excellent, good, average, poor):
+        self.excellent = excellent
+        self.good = good
+        self.average = average
+        self.poor = poor
+    excellent_utilization =< 0.10
+    good_utilization =< 0.30
+    average_utilization =< 0.60
+    poor_utilization =< 1
+    
+class CreditAccount:
+    def __init__(self, cardName, user_APR, user_AvgUtilization, spendingLimit):
+        self.cardName = cardName
+        self.user_APR = user_APR
+        self.user_AvgUtilization = user_AvgUtilization
+        self.spendingLimit = spendingLimit
