e6500f452c3951e632e2868f1b14a4b4da71f1ac
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 03:58:28 -0500

    Update CreditLine.py
    Fixed multi-line comment issue

--- CreditLine.py
+++ CreditLine.py
@@ -33,8 +33,8 @@ class CreditAccount:
         self.spendingLimit = spendingLimit
 # Create and store instance of user's individual card line 
 creditAccount_1 = CreditAccount(cardName, user_APR, AvgUtilization, spendingLimit)
-"""This function will display and ask for confirmation regarding user's individual credit card line. If the  
-information was correct, the componenets are cleared to be used in the next python script: "RealInterestCosts.py""""
+#This function will display and ask for confirmation regarding user's individual credit card line. If the  
+#information was correct, the componenets are cleared to be used in the next python script: "RealInterestCosts.py"
 def confirmation(CreditAccount_1):
     print "Reviewing your credit line information..."  
     print "Credit Card Name: %s" % cardName
