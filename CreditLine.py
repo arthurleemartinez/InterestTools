eee7d3e1f48b8b34495f6519abadef4b788be080
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-30 03:45:09 -0500

    Update CreditLine.py
    Add information confirmation step, and imported a script restart function for if condition

--- CreditLine.py
+++ CreditLine.py
@@ -1,6 +1,16 @@
+import sys
+import os
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+    
 # Initialize User Interface
-print "This program will ask you a few basic questions about your credit line "
+print "This program will ask you a few basic questions about your credit line in order to show you the real impact of your interest rates and spending habits."
 # Function for terminal's user interface
+cardName = raw_input("Please enter your card name (e.g. Mastercard).")
 def APR():
     percentage = raw_input("What is your current annual APR?")
     decimalString = (percentage.strip(["%"])):
@@ -24,6 +34,29 @@ class CreditAccount:
         self.spendingLimit = spendingLimit
     
 # Create and store instance of user's individual card line 
-creditAccount_1 = CreditAccount("Discover", user_APR, AvgUtilization, spendingLimit)
-# Display user's individual card line
-print creditAccount_1
+creditAccount_1 = CreditAccount(cardName, user_APR, AvgUtilization, spendingLimit)
+
+"""This function will display and ask for confirmation regarding user's individual credit card line. If the  
+   information was correct, the componenets are cleared to be used in the next python script: "RealInterestCosts.py""""
+def confirmation(CreditAccount_1):
+    print "Reviewing your credit line information..."  
+    print "Credit Card Name: %s" % cardName
+    print "Spending Limit: %d" % spendingLimit
+    print "Your APR: %d" % user_APR
+    confirmation_answer = raw_input("Is this information correct? (Answer y or n)")
+    if (confirmation_answer == "y"):
+        boolean_answer = True
+    else:
+        boolean_answer = False
+    if boolean_answer = False:
+        print "Restarting shell.."
+        restart_program()
+        
+
+if __name__ == "__main__":
+    answer = raw_input("Do you want to restart this program ? ")
+    if answer.lower().strip() in "y yes".split():
+        
+        
+    
+    Your credit line information: creditAccount_1
