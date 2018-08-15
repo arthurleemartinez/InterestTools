7720bcd741ce89c37975e40662c122084b0a0f45
Arthur Martinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-08-15 10:01:16 -0500

    Update high_interest_loan.py
    Fixed function returning none bug

--- high_interest_loan.py
+++ high_interest_loan.py
@@ -12,7 +12,7 @@ def get_user_apr() -> float:
 user_apr = get_user_apr()
 print('Your interest rate is {}%.'.format(user_apr))
 def get_user_daily_interest() -> float:
-    user_daily_interest: float = 2 * user_principle * (user_apr / 365 / 100)
+    user_daily_interest: float = user_principle * (user_apr / 365 / 100)
     return user_daily_interest
 daily_interest: float = get_user_daily_interest()
 print('You are spending ${} on interest every day.'.format(daily_interest))
@@ -28,22 +28,22 @@ def get_pay_check_cost() -> float:
     elif monthly_interest_cost > 27:
         pay_period_interest_cost = monthly_interest_cost
     return round(pay_period_interest_cost, 2)
-pay_check_cost: float = round(get_pay_check_cost(),2)
+
+pay_check_cost: float = 2*round(get_pay_check_cost(),2)
 user_pay_check_amount = float(input("How much do you get paid every pay period?"))
 def get_paycheck_percentage():
-    pay_check_percentage1: float = pay_check_cost / user_pay_check_amount * 100
-    return round(pay_check_percentage1,2)
-pay_check_percentage = get_paycheck_percentage()
+    pay_check_percentage1: float = round((pay_check_cost / user_pay_check_amount * 100), 2)
+    return pay_check_percentage1
+pay_check_percentage = round(get_paycheck_percentage() , 2)
 # determine how much money you will save on each minimum payment if you pay down a certain principle amount.
 def pay_down_implications():
-    if user_plans != True:
-        pass
-    else:
         payment_more_than_minimum: float = float(str(input("How much do you want to pay toward the principle this pay-check?")))
-        new_daily_interest = 2*(user_principle - payment_more_than_minimum)/365/100*user_apr
-        new_pay_check_percentage = (new_daily_interest * user_pay_period) / user_pay_check_amount
-        message = print('If you make a payment of ${} before the next due date, this loan will begin eating up %{} of your pay-checks.'.format(payment_more_than_minimum, new_pay_check_percentage))
+        new_daily_interest = (user_principle - payment_more_than_minimum)/365/100*user_apr
+        new_pay_check_percentage: float = round((100*(new_daily_interest * user_pay_period) / user_pay_check_amount), 2)
+        new_pay_check_cost:float = round((new_pay_check_percentage/100*user_pay_check_amount) , 2)
+        message = 'If you make a payment of ${} before the next due date, this loan will change to taking up %{} of your pay-checks. This means that your next payment would be ${}'.format(payment_more_than_minimum, new_pay_check_percentage, new_pay_check_cost)
         return message
 print('Your pay period is {} days long'.format(user_pay_period))
 print('You are spending approximately ${} per pay-check on this loan'.format(pay_check_cost))
 print('Your high-interest loan will eat up %{} of your paycheck this pay period.'.format(pay_check_percentage))
+print(pay_down_implications())
